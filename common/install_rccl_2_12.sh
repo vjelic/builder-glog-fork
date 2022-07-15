1114c01da60d1143223e692b897720c8ad3c8857
Pruthvi Madugundu <pruthvigithub@gmail.com>
2022-07-14 23:05:22 -0700

    Changes to install cmake

--- common/install_rccl_2_12.sh
+++ common/install_rccl_2_12.sh
@@ -12,7 +12,33 @@ export NCCL_IB_PCI_RELAXED_ORDERING=1
 export NCCL_NET_GDR_LEVEL=3
 
 # clean up dependencies no longer needed for RCCL 2.12
-RUN cd / && find /usr -name librccl-net.so* | xargs rm -rf
+cd / && find /usr -name librccl-net.so* | xargs rm -rf
+
+#install cmake 3.8 version
+ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
+case "$ID" in
+  ubuntu)
+    apt update -y
+    ;;
+  centos)
+    yum autoremove -y cmake
+    yum update -y
+    rpm -e --nodeps rccl rccl-devel
+    yum -y install gcc gcc-c++ wget make vim
+    cd ~
+    wget http://www.cmake.org/files/v3.8/cmake-3.8.2.tar.gz
+    tar -zxvf cmake-3.8.2.tar.gz
+    cd cmake-3.8.2
+    ./bootstrap
+    make install
+    cp -f ./bin/cmake ./bin/cpack ./bin/ctest /bin
+    ;;
+  *)
+    echo "Unable to determine OS..."
+    exit 1
+    ;;
+esac
+
 
 cd ~ && git clone https://github.com/ROCmSoftwarePlatform/rccl.git
 cd ~/rccl && git reset --hard 8c3c8b7 && ./install.sh -id && cd build/release && make package
