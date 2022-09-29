7ae4c29acfb1ebb013f5098f65653c63744d98c7
NISHIY-EKSDEE <slava.astra@yahoo.com>
2022-09-29 09:26:08 +0200

    Add pytorch rocm arch default value

--- conda/build_docker.sh
+++ conda/build_docker.sh
@@ -33,7 +33,8 @@ case ${GPU_ARCH_TYPE} in
     BASE_TARGET=rocm_base
     DOCKER_TAG=rocm${GPU_ARCH_VERSION}
     GPU_IMAGE=rocm/dev-centos-7:${GPU_ARCH_VERSION}
-    DOCKER_GPU_BUILD_ARG="--build-arg ROCM_VERSION=${GPU_ARCH_VERSION}"
+    PYTORCH_ROCM_ARCH="gfx900;gfx906;gfx908"
+    DOCKER_GPU_BUILD_ARG="--build-arg ROCM_VERSION=${GPU_ARCH_VERSION} --build-arg PYTORCH_ROCM_ARCH=${PYTORCH_ROCM_ARCH}"
     ;;
   *)
     echo "ERROR: Unrecognized GPU_ARCH_TYPE: ${GPU_ARCH_TYPE}"
