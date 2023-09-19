cd2cd788a44d39fec15e62100e14bd6e066d8278
Jithun Nair <jithun.nair@amd.com>
2023-09-19 23:21:21 +0000

    Add rccl install step to use rccl with fixes

---
+++ common/install_rccl.sh
@@ -0,0 +1,4 @@
+yum remove -y rccl
+
+yum install -y https://repo.radeon.com/rocm/manylinux/rocm-rel-5.7/.private_rccl_5.7/rccl-2.17.1-e896f0d.el7.x86_64.rpm
+yum install -y https://repo.radeon.com/rocm/manylinux/rocm-rel-5.7/.private_rccl_5.7/rccl-devel-2.17.1-e896f0d.el7.x86_64.rpm
