caca055744ce36e438966852de232a4257d223fb
Andrey Talman <atalman@fb.com>
2024-06-25 11:54:28 -0400

    Remove cpython 3.7.5 from Docker (#1885)
    * Remove cpython 3.7.5 from Docker
    
    * Advance build.sh scrript
    
    * fix_folder
    
    * More_py37_refactor
    
    * wrong_file
    
    Cherry-picked from https://github.com/pytorch/builder/commit/9b5c267826bc37713ebaa4b4c0d3f01cec390d24

--- common/install_cpython.sh
+++ common/install_cpython.sh
@@ -5,7 +5,7 @@ PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python
 GET_PIP_URL=https://bootstrap.pypa.io/get-pip.py
 
 # Python versions to be installed in /opt/$VERSION_NO
-CPYTHON_VERSIONS=${CPYTHON_VERSIONS:-"3.7.5 3.8.1 3.9.0 3.10.1 3.11.0 3.12.0"}
+CPYTHON_VERSIONS=${CPYTHON_VERSIONS:-"3.8.1 3.9.0 3.10.1 3.11.0 3.12.0 3.13.0"}
 
 function check_var {
     if [ -z "$1" ]; then
