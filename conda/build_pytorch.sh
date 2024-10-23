dcce09c0469036a32b2aad7409b6e5844b840547
Viacheslav Astrakhantsev <48382010+NISHIY-EKSDEE@users.noreply.github.com>
2024-10-23 22:56:56 +0200

    Add aotriton dependency. Pin exact triton version from triton_version.txt file

--- conda/build_pytorch.sh
+++ conda/build_pytorch.sh
@@ -321,9 +321,11 @@ elif [[ "$gpu_arch_type" == 'rocm' ]]; then
 
     if [[ "$OSTYPE" != "msys" ]]; then
         # TRITON_SHORTHASH=$(cut -c1-10 $pytorch_rootdir/.ci/docker/ci_commit_pins/triton-rocm.txt)
-        # TRITON_VERSION=$(cat $pytorch_rootdir/.ci/docker/triton_version.txt)
         # export CONDA_TRITON_CONSTRAINT="    - pytorch-triton-rocm==${TRITON_VERSION}+${TRITON_SHORTHASH}"
-        export CONDA_TRITON_CONSTRAINT="    - pytorch-triton-rocm >=2.0"
+        TRITON_VERSION=$(cat $pytorch_rootdir/.ci/docker/triton_version.txt)
+        AOTRITON_VERSION=$(head -n 1 $pytorch_rootdir/.ci/docker/aotriton_version.txt)
+        export CONDA_TRITON_CONSTRAINT="    - pytorch-triton-rocm==${TRITON_VERSION}"
+        export CONDA_AOTRITON_CONSTRAINT="    - aotriton==${AOTRITON_VERSION}"
     fi
 
     if [[ -n "$PYTORCH_BUILD_STRING_A" ]]; then
